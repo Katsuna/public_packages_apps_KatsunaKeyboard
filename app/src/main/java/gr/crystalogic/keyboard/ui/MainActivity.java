1819b76a2f5a5ae952c5407f3e73bd6bcea58b43
alkis <a.kafkis@gmail.com>
2016-05-12 12:05:30 +0300

    initial demo

---
+++ app/src/main/java/gr/crystalogic/keyboard/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package gr.crystalogic.keyboard.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import gr.crystalogic.keyboard.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
