c63e47f3a26c72035b50545ded1312f5aa7353dc
alkis <a.kafkis@gmail.com>
2016-06-03 11:00:14 +0300

    font set to roboto medium

---
+++ app/src/main/java/gr/crystalogic/keyboard/Application.java
@@ -0,0 +1,11 @@
+package gr.crystalogic.keyboard;
+
+import gr.crystalogic.keyboard.utils.FontsOverride;
+
+public final class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/Roboto-Medium.ttf");
+    }
+}
