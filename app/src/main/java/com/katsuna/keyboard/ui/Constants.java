d50b9d17b197cb5182315fcb5570eba9981addcc
alkis <a.kafkis@gmail.com>
2016-08-29 22:36:31 +0300

    Guide profile v4 style

--- app/src/main/java/com/katsuna/keyboard/ui/Constants.java
+++ app/src/main/java/com/katsuna/keyboard/ui/Constants.java
@@ -2,4 +2,5 @@ package com.katsuna.keyboard.ui;
 
 class Constants {
     static final int KEYCODE_LANGUAGE_SWITCH = -101;
+    static final int KEYCODE_SPACE = 32;
 }
--- app/src/main/java/com/katsuna/keyboard/ui/LatinKeyboardView.java
+++ app/src/main/java/com/katsuna/keyboard/ui/LatinKeyboardView.java
@@ -1,7 +1,11 @@
 package com.katsuna.keyboard.ui;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.util.AttributeSet;
 
@@ -9,12 +13,16 @@ import com.katsuna.keyboard.R;
 
 public class LatinKeyboardView extends KeyboardView {
 
+    private Context mContext;
+
     public LatinKeyboardView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
     }
 
     public LatinKeyboardView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mContext = context;
     }
 
     public boolean setShiftKey(boolean shifted, boolean qwertyKeyboard) {
@@ -43,5 +51,28 @@ public class LatinKeyboardView extends KeyboardView {
             }
         }
         return false;
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        for (Keyboard.Key key : getKeyboard().getKeys()) {
+            if (key.codes[0] == Constants.KEYCODE_SPACE) {
+
+                int margin = 4;
+                Drawable dr = ContextCompat.getDrawable(mContext, R.drawable.space_bar_bg);
+                dr.setBounds(key.x + margin, key.y + margin, key.x + key.width - margin, key.y + key.height - margin);
+                dr.draw(canvas);
+
+                //calculate bounds
+                Drawable dr2 = ContextCompat.getDrawable(mContext, R.drawable.ic_space_bar_black_36dp);
+                int left = key.x + key.width / 2 - dr2.getIntrinsicWidth() / 2;
+                int top = key.y + key.height / 2 - dr2.getIntrinsicHeight() / 2;
+
+                dr2.setBounds(left, top, left + dr2.getIntrinsicWidth(), top + dr2.getIntrinsicHeight());
+                dr2.draw(canvas);
+            }
+        }
     }
 }
